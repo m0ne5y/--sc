9730d4b57664434a5a60c27336c8b37667078038
h3sitate <161268161+m0ne5y@users.noreply.github.com>
2025-04-03 11:29:48 +0800

    Add files via upload

---
+++ main/models.py
@@ -0,0 +1,346 @@
+#coding:utf-8
+__author__ = "ila"
+from django.db import models
+
+from .model import BaseModel
+
+from datetime import datetime
+
+
+
+class yonghu(BaseModel):
+    __doc__ = u'''yonghu'''
+    __tablename__ = 'yonghu'
+
+    __loginUser__='zhanghao'
+
+
+    __authTables__={}
+    __authPeople__='是'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __loginUserColumn__='zhanghao'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='否'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='否'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='否'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='否'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    zhanghao=models.CharField ( max_length=255,null=False,unique=True, verbose_name='账号' )
+    xingming=models.CharField ( max_length=255,null=False, unique=False, verbose_name='姓名' )
+    mima=models.CharField ( max_length=255,null=False, unique=False, verbose_name='密码' )
+    xingbie=models.CharField ( max_length=255, null=True, unique=False, verbose_name='性别' )
+    touxiang=models.TextField   (  null=True, unique=False, verbose_name='头像' )
+    dianhua=models.CharField ( max_length=255, null=True, unique=False, verbose_name='电话' )
+    '''
+    zhanghao=VARCHAR
+    xingming=VARCHAR
+    mima=VARCHAR
+    xingbie=VARCHAR
+    touxiang=Text
+    dianhua=VARCHAR
+    '''
+    class Meta:
+        db_table = 'yonghu'
+        verbose_name = verbose_name_plural = '用户'
+class youxixinxi(BaseModel):
+    __doc__ = u'''youxixinxi'''
+    __tablename__ = 'youxixinxi'
+
+
+
+    __authTables__={}
+    __authPeople__='否'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='否'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='是'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='是'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='是'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    youxibianhao=models.CharField ( max_length=255, null=True,unique=True, verbose_name='游戏编号' )
+    youximingcheng=models.CharField ( max_length=255,null=False, unique=False, verbose_name='游戏名称' )
+    youxileixing=models.CharField ( max_length=255,null=False, unique=False, verbose_name='游戏类型' )
+    fengmian=models.TextField   (  null=True, unique=False, verbose_name='封面' )
+    shipinjieshao=models.TextField   (  null=True, unique=False, verbose_name='视频介绍' )
+    biaoqian=models.CharField ( max_length=255, null=True, unique=False, verbose_name='标签' )
+    zongpingfen=models.IntegerField  (  null=True, unique=False, verbose_name='总评分' )
+    danjia=models.IntegerField  (  null=True, unique=False, verbose_name='单价' )
+    youxichangshang=models.CharField ( max_length=255, null=True, unique=False, verbose_name='游戏厂商' )
+    youxixiangqing=models.TextField   (  null=True, unique=False, verbose_name='游戏详情' )
+    gengxinshijian=models.DateField   (  null=True, unique=False, verbose_name='更新时间' )
+    clicktime=models.DateTimeField  (auto_now=True,  null=True, unique=False, verbose_name='最近点击时间' )
+    clicknum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='点击次数' )
+    discussnum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='评论数' )
+    storeupnum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='收藏数' )
+    '''
+    youxibianhao=VARCHAR
+    youximingcheng=VARCHAR
+    youxileixing=VARCHAR
+    fengmian=Text
+    shipinjieshao=Text
+    biaoqian=VARCHAR
+    zongpingfen=Integer
+    danjia=Integer
+    youxichangshang=VARCHAR
+    youxixiangqing=Text
+    gengxinshijian=Date
+    clicktime=DateTime
+    clicknum=Integer
+    discussnum=Integer
+    storeupnum=Integer
+    '''
+    class Meta:
+        db_table = 'youxixinxi'
+        verbose_name = verbose_name_plural = '游戏信息'
+class youxileixing(BaseModel):
+    __doc__ = u'''youxileixing'''
+    __tablename__ = 'youxileixing'
+
+
+
+    __authTables__={}
+    __authPeople__='否'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='否'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='否'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='否'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='否'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    youxileixing=models.CharField ( max_length=255, null=True, unique=False, verbose_name='游戏类型' )
+    '''
+    youxileixing=VARCHAR
+    '''
+    class Meta:
+        db_table = 'youxileixing'
+        verbose_name = verbose_name_plural = '游戏类型'
+class youxipingfen(BaseModel):
+    __doc__ = u'''youxipingfen'''
+    __tablename__ = 'youxipingfen'
+
+
+
+    __authTables__={'zhanghao':'yonghu',}
+    __authPeople__='否'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='否'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='否'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='否'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='否'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    youxibianhao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='游戏编号' )
+    youximingcheng=models.CharField ( max_length=255,null=False, unique=False, verbose_name='游戏名称' )
+    youxileixing=models.CharField ( max_length=255,null=False, unique=False, verbose_name='游戏类型' )
+    fengmian=models.TextField   (  null=True, unique=False, verbose_name='封面' )
+    youxichangshang=models.CharField ( max_length=255, null=True, unique=False, verbose_name='游戏厂商' )
+    danjia=models.IntegerField  (  null=True, unique=False, verbose_name='单价' )
+    zongpingfen=models.IntegerField  ( null=False, unique=False, verbose_name='评分' )
+    pingfenshijian=models.DateTimeField  (  null=True, unique=False, verbose_name='评分时间' )
+    zhanghao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='账号' )
+    xingming=models.CharField ( max_length=255, null=True, unique=False, verbose_name='姓名' )
+    crossuserid=models.BigIntegerField  (  null=True, unique=False, verbose_name='跨表用户id' )
+    crossrefid=models.BigIntegerField  (  null=True, unique=False, verbose_name='跨表主键id' )
+    '''
+    youxibianhao=VARCHAR
+    youximingcheng=VARCHAR
+    youxileixing=VARCHAR
+    fengmian=Text
+    youxichangshang=VARCHAR
+    danjia=Integer
+    zongpingfen=Integer
+    pingfenshijian=DateTime
+    zhanghao=VARCHAR
+    xingming=VARCHAR
+    crossuserid=BigInteger
+    crossrefid=BigInteger
+    '''
+    class Meta:
+        db_table = 'youxipingfen'
+        verbose_name = verbose_name_plural = '游戏评分'
+class youxigoumai(BaseModel):
+    __doc__ = u'''youxigoumai'''
+    __tablename__ = 'youxigoumai'
+
+
+
+    __authTables__={'zhanghao':'yonghu',}
+    __authPeople__='否'#用户表，表属性loginUserColumn对应的值就是用户名字段，mima就是密码字段
+    __sfsh__='否'#表sfsh(是否审核，”是”或”否”)字段和sfhf(审核回复)字段，后台列表(page)的操作中要多一个”审核”按钮，点击”审核”弹出一个页面，包含”是否审核”和”审核回复”，点击确定调用update接口，修改sfsh和sfhf两个字段。
+    __authSeparate__='否'#后台列表权限
+    __thumbsUp__='否'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='否'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='否'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    __foreEndListAuth__='否'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    __foreEndList__='否'#表属性[foreEndList]前台list:和后台默认的list列表页相似,只是摆在前台,否:指没有此页,是:表示有此页(不需要登陆即可查看),前要登:表示有此页且需要登陆后才能查看
+    __isAdmin__='否'#表属性isAdmin=”是”,刷出来的用户表也是管理员，即page和list可以查看所有人的考试记录(同时应用于其他表)
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    dingdanbianhao=models.CharField ( max_length=255, null=True,unique=True, verbose_name='订单编号' )
+    youxibianhao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='游戏编号' )
+    youximingcheng=models.CharField ( max_length=255,null=False, unique=False, verbose_name='游戏名称' )
+    fengmian=models.TextField   (  null=True, unique=False, verbose_name='封面' )
+    youxileixing=models.CharField ( max_length=255,null=False, unique=False, verbose_name='游戏类型' )
+    youxichangshang=models.CharField ( max_length=255, null=True, unique=False, verbose_name='游戏厂商' )
+    danjia=models.IntegerField  (  null=True, unique=False, verbose_name='单价' )
+    xiadanshijian=models.DateTimeField  (  null=True, unique=False, verbose_name='下单时间' )
+    zhanghao=models.CharField ( max_length=255, null=True, unique=False, verbose_name='账号' )
+    xingming=models.CharField ( max_length=255, null=True, unique=False, verbose_name='姓名' )
+    ispay=models.CharField ( max_length=255, null=True, unique=False,default='未支付', verbose_name='是否支付' )
+    '''
+    dingdanbianhao=VARCHAR
+    youxibianhao=VARCHAR
+    youximingcheng=VARCHAR
+    fengmian=Text
+    youxileixing=VARCHAR
+    youxichangshang=VARCHAR
+    danjia=Integer
+    xiadanshijian=DateTime
+    zhanghao=VARCHAR
+    xingming=VARCHAR
+    ispay=VARCHAR
+    '''
+    class Meta:
+        db_table = 'youxigoumai'
+        verbose_name = verbose_name_plural = '游戏购买'
+class chat(BaseModel):
+    __doc__ = u'''chat'''
+    __tablename__ = 'chat'
+
+
+
+    __authTables__={}
+    __foreEndListAuth__='是'#前台列表权限foreEndListAuth[是/否]；当foreEndListAuth=是，刷的表新增用户字段userid，前台list列表接口仅能查看自己的记录和add接口后台赋值userid的值
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    userid=models.BigIntegerField  ( null=False, unique=False, verbose_name='用户id' )
+    adminid=models.BigIntegerField  (  null=True, unique=False, verbose_name='管理员id' )
+    ask=models.TextField   (  null=True, unique=False, verbose_name='提问' )
+    reply=models.TextField   (  null=True, unique=False, verbose_name='回复' )
+    isreply=models.IntegerField  (  null=True, unique=False, verbose_name='是否回复' )
+    '''
+    userid=BigInteger
+    adminid=BigInteger
+    ask=Text
+    reply=Text
+    isreply=Integer
+    '''
+    class Meta:
+        db_table = 'chat'
+        verbose_name = verbose_name_plural = '在线咨询'
+class newstype(BaseModel):
+    __doc__ = u'''newstype'''
+    __tablename__ = 'newstype'
+
+
+
+    __authTables__={}
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    typename=models.CharField ( max_length=255,null=False, unique=False, verbose_name='分类名称' )
+    '''
+    typename=VARCHAR
+    '''
+    class Meta:
+        db_table = 'newstype'
+        verbose_name = verbose_name_plural = '公告信息分类'
+class news(BaseModel):
+    __doc__ = u'''news'''
+    __tablename__ = 'news'
+
+
+
+    __authTables__={}
+    __thumbsUp__='是'#表属性thumbsUp[是/否]，新增thumbsupnum赞和crazilynum踩字段
+    __intelRecom__='是'#智能推荐功能(表属性：[intelRecom（是/否）],新增clicktime[前端不显示该字段]字段（调用info/detail接口的时候更新），按clicktime排序查询)
+    __browseClick__='是'#表属性[browseClick:是/否]，点击字段（clicknum），调用info/detail接口的时候后端自动+1）、投票功能（表属性[vote:是/否]，投票字段（votenum）,调用vote接口后端votenum+1
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    title=models.CharField ( max_length=255,null=False, unique=False, verbose_name='标题' )
+    introduction=models.TextField   (  null=True, unique=False, verbose_name='简介' )
+    typename=models.CharField ( max_length=255, null=True, unique=False, verbose_name='分类名称' )
+    name=models.CharField ( max_length=255, null=True, unique=False, verbose_name='发布人' )
+    headportrait=models.TextField   (  null=True, unique=False, verbose_name='头像' )
+    clicknum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='点击次数' )
+    clicktime=models.DateTimeField  (auto_now=True,  null=True, unique=False, verbose_name='最近点击时间' )
+    thumbsupnum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='赞' )
+    crazilynum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='踩' )
+    storeupnum=models.IntegerField  (  null=True, unique=False,default='0', verbose_name='收藏数' )
+    picture=models.TextField   ( null=False, unique=False, verbose_name='图片' )
+    content=models.TextField   ( null=False, unique=False, verbose_name='内容' )
+    '''
+    title=VARCHAR
+    introduction=Text
+    typename=VARCHAR
+    name=VARCHAR
+    headportrait=Text
+    clicknum=Integer
+    clicktime=DateTime
+    thumbsupnum=Integer
+    crazilynum=Integer
+    storeupnum=Integer
+    picture=Text
+    content=Text
+    '''
+    class Meta:
+        db_table = 'news'
+        verbose_name = verbose_name_plural = '公告信息'
+class storeup(BaseModel):
+    __doc__ = u'''storeup'''
+    __tablename__ = 'storeup'
+
+
+
+    __authTables__={}
+    __authSeparate__='是'#后台列表权限
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    userid=models.BigIntegerField  ( null=False, unique=False, verbose_name='用户id' )
+    refid=models.BigIntegerField  (  null=True, unique=False, verbose_name='商品id' )
+    tablename=models.CharField ( max_length=255, null=True, unique=False, verbose_name='表名' )
+    name=models.CharField ( max_length=255,null=False, unique=False, verbose_name='名称' )
+    picture=models.TextField   (  null=True, unique=False, verbose_name='图片' )
+    type=models.CharField ( max_length=255, null=True, unique=False,default='1', verbose_name='类型' )
+    inteltype=models.CharField ( max_length=255, null=True, unique=False, verbose_name='推荐类型' )
+    remark=models.CharField ( max_length=255, null=True, unique=False, verbose_name='备注' )
+    '''
+    userid=BigInteger
+    refid=BigInteger
+    tablename=VARCHAR
+    name=VARCHAR
+    picture=Text
+    type=VARCHAR
+    inteltype=VARCHAR
+    remark=VARCHAR
+    '''
+    class Meta:
+        db_table = 'storeup'
+        verbose_name = verbose_name_plural = '收藏表'
+class discussyouxixinxi(BaseModel):
+    __doc__ = u'''discussyouxixinxi'''
+    __tablename__ = 'discussyouxixinxi'
+
+
+
+    __authTables__={}
+    addtime = models.DateTimeField(auto_now_add=False, verbose_name=u'创建时间')
+    refid=models.BigIntegerField  ( null=False, unique=False, verbose_name='关联表id' )
+    userid=models.BigIntegerField  ( null=False, unique=False, verbose_name='用户id' )
+    avatarurl=models.TextField   (  null=True, unique=False, verbose_name='头像' )
+    nickname=models.CharField ( max_length=255, null=True, unique=False, verbose_name='用户名' )
+    content=models.TextField   ( null=False, unique=False, verbose_name='评论内容' )
+    reply=models.TextField   (  null=True, unique=False, verbose_name='回复内容' )
+    '''
+    refid=BigInteger
+    userid=BigInteger
+    avatarurl=Text
+    nickname=VARCHAR
+    content=Text
+    reply=Text
+    '''
+    class Meta:
+        db_table = 'discussyouxixinxi'
+        verbose_name = verbose_name_plural = '游戏信息评论表'
